fc1c9009c5235487ab0d8363bd3082199bbdf7d7
RispaJoseph <rizpahjoseph@gmail.com>
2025-08-30 04:51:51 +0530

    netlify deployment

--- backend/settings.py
+++ backend/settings.py
@@ -207,6 +207,8 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True 
 
+ORS_ALLOW_ALL_ORIGINS = True
+
 CSRF_TRUSTED_ORIGINS = [
     # "http://localhost:5173",
     # "http://127.0.0.1:5173",
