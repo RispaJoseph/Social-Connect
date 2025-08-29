8de6e2de4eab8207e43968e3bd27fca48e44042b
RispaJoseph <rizpahjoseph@gmail.com>
2025-08-30 04:47:52 +0530

    netlify deployment

--- backend/settings.py
+++ backend/settings.py
@@ -199,16 +199,18 @@ FRONTEND_URL = "http://localhost:5173"
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  
-    "http://127.0.0.1:5173",
+    # "http://localhost:5173",  
+    # "http://127.0.0.1:5173",
+    "https://rispa-social-connect.netlify.app",
 ]
 
 
 CORS_ALLOW_CREDENTIALS = True 
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    # "http://localhost:5173",
+    # "http://127.0.0.1:5173",
+    "https://rispa-social-connect.netlify.app",
     ]
 
 
