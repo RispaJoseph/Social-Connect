ccf8c515461b02e53e24c5c0ae9b4789543ed5c9
RispaJoseph <rizpahjoseph@gmail.com>
2025-08-30 22:27:30 +0530

    Netlify update

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,9 @@ DEBUG = config("DEBUG", default=True, cast=bool)
 
 
 # ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*").split(",")
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
+# ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
